101d8b1a8976216c49c2f5d1a8592a721d70263e
obseanfrom <109475419+obseanfrom@users.noreply.github.com>
2024-07-22 19:38:49 -0500

    Correction to uploaded file
    Accidentally committed a backup file from another directory, uploading the primary backup and correcting any errors.

--- app.R
+++ app.R
@@ -1,20 +1,49 @@
+library(tidyverse)
+library(ggplot2)
 library(shiny)
-covid_19 <<- read.csv("covid_19.csv")[,c(1,6,8)]
 
-
-ui <- fluidPage(
-  covid_19 <- read.csv("covid_19.csv")[,c(1,6,8)],
-  titlePanel("Covid 19 cases"),
-  sidebarLayout(
-    sidebarPanel(
-      sliderInput("country","cases", 0, 100000, c(25, 40)),
-      selectInput("countryInput","country",
-                  choices = covid_19$country)
-    )
-  )
-)
+vars <- setdiff(unique(mpg$class), "Class")
+vars.2 <- setdiff(unique(mpg$manufacturer), "Manufacturer")
 
 #create server function
-server <- function(input, output) {}
+server <- function(input, output, session) {
+  current.data <- reactive({
+    data.1 <- subset(mpg, class == input$Class & manufacturer == input$Manufacturer)
+  })
+  current.data.2 <- reactive({
+    data.1 <- subset(mpg, manufacturer == input$Manufacturer)
+  })
+  output$table <- renderTable(
+    mpg %>% 
+      group_by(manufacturer) %>%
+      pivot_longer(names_to = class, values_to = manufacturer)
+  )
+  output$hist <- renderPlot(
+    ggplot(current.data()) + 
+      geom_histogram(aes(x = hwy)) +
+      ggtitle(paste(input$Class, "highway miles per gallon built by", input$Manufacturer)) +
+      xlab("Highway miles per gallon")
+  )
+  output$bar <- renderPlot(
+    ggplot(current.data.2()) +
+      geom_bar(aes(x = class)) +
+      ggtitle("Class distribution by selected manufacturer") +
+      xlab("Class")
+  )
+}
+
+ui <- fluidPage(
+  titlePanel("Highway MPG by Class"),
+  fluidRow(
+    column(3,
+      selectInput("Class","class", vars),
+      selectInput("Manufacturer","manufacturer", vars.2)),
+    column(9,
+      plotOutput('hist'),
+      plotOutput('bar'),
+      tableOutput('table')
+      )
+    )
+)
 
 shinyApp(ui = ui, server = server)
